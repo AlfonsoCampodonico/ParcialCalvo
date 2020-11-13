bdcb6b19dffdae4243207e7771633c97dee7c316
Alfonso Campodonico <38387367+AlfonsoCampodonico@users.noreply.github.com>
2020-11-13 13:04:30 -0300

    Create pregunta6.cpp

---
+++ pregunta6.cpp
@@ -0,0 +1,23 @@
+/*
+Enunciado:
+Enumere los sectores en lo que se divide la memoria de un programa 
+y describa brevemente cuál es su propósito.
+*/
+
+/*
+Respuesta:
+Segmento de  Codigo: Donde se ejecuta el programa.
+Segmento de Datos: Donde se ubican las variables del programa principal
+Segmento de Pila: Segmento donde se alojan los llamados a las funciones
+Segmento Exta (Heap) : Sirva para utilizar la memoria dinamica
+*/
+
+/*
+Valor: 0.9/1.0
+*/
+
+/*
+Comentarios:
+El Stack almacenará el contenido de las variables locales 
+y parámetros en cada invocación de funciones (incluyendo las del main)
+*/
