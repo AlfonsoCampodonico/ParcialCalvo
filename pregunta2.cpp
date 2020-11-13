595367394cf07417d1ac9876759bbef625660332
Alfonso Campodonico <38387367+AlfonsoCampodonico@users.noreply.github.com>
2020-11-13 12:51:55 -0300

    Create pregunta2.cpp

---
+++ pregunta2.cpp
@@ -0,0 +1,32 @@
+/*
+2.Ejemplificar en un segmento de código cómo dos 
+punteros de distinto tipo leen y escriben de 
+distinta manera el mismo área de memoria.
+*/
+
+int main()
+{
+    int x = 65;
+    int* p;
+    char* q;
+    p = &x;
+    q = (char*)&x;
+
+    std::cout << "*p : " << *p << std::endl;
+    std::cout << "*q : " << *q << std::endl;
+    *p = 66;
+    std::cout << "*p : " << *p << std::endl;
+    std::cout << "*q : " << *q << std::endl;
+    *q = 'F';
+    std::cout << "*p : " << *p << std::endl;
+    std::cout << "*q : " << *q << std::endl;
+	
+    return 0;
+}
+
+
+
+/*
+Comentarios:
+
+*/
