50e3fefb24ccea0941c3465c777ac8aabd31958c
Alfonso Campodonico <38387367+AlfonsoCampodonico@users.noreply.github.com>
2020-11-13 12:56:09 -0300

    Create pregunta4.cpp

---
+++ pregunta4.cpp
@@ -0,0 +1,24 @@
+/*
+Escribir un segmento de código que utilice un puntero para leer 
+y modificar el valor de una variable alojada en el stack de la 
+función main desde otra función.
+*/
+void f1(int* b)
+{
+	*b = 54;
+}
+int main()
+{
+	int a = 5;
+	std::cout << "a :" << a << std::endl;
+	int* b = &a;
+	f1(b);
+
+	std::cout << "*b: " << *b << std::endl;
+	std::cout << "a :" << a << std::endl;
+	return 0;
+}
+
+/*
+Comentarios:
+*/
