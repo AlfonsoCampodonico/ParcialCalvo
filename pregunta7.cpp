2635ee66ab2f1f6bc4aa225036a1567f93cf0caf
Alfonso Campodonico <38387367+AlfonsoCampodonico@users.noreply.github.com>
2020-11-13 13:06:33 -0300

    Create pregunta7.cpp

---
+++ pregunta7.cpp
@@ -0,0 +1,26 @@
+/*
+Enunciado:
+Escribir un programa que deje perdidos (memory leak) 8 bytes de memoria no contigua (en 3 bloques).
+*/
+
+int main()
+{
+    int* a;
+    a = new int;
+    char* b;
+    b = new char;
+
+    char* c;
+    c = new char[3];
+
+	
+    return 0;
+}
+
+/*
+Valor: 1.0/1.0
+*/
+
+/*
+Comentarios:
+*/
